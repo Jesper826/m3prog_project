f80108655f87a85be8c9d47f377652391af54621
JespervanNiekerk <jespervanniekerk1@gmail.com>
2025-02-10 13:02:38 +0100

    01 gemaakt van de eerste opdracht

---
+++ public/01/its-me.php
@@ -0,0 +1,5 @@
+<?php
+echo "Mijn favorite film is James bond (ik ben alleen de naam vergeten van de specifieke film).";
+echo "Mijn favorite game is Lego Star Wars.";
+echo "Ik zit in SD1C";
+?>
---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php
+echo "<h1>Kopje</h1>";
+echo "<p>Paragraaf</p>";
+echo "<img src='images/bram2.png' alt='bram'>";
+?>
