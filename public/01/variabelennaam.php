c8d3584c8aed1a973cdf2ef487d30351f0f67a18
JespervanNiekerk <jespervanniekerk1@gmail.com>
2025-02-13 12:30:03 +0100

    create variabelennaam.php

---
+++ public/01/variabelennaam.php
@@ -0,0 +1,22 @@
+<?php
+    $auto2 = "BMW";
+    echo "<br>$auto2";
+    $_naam = "Bram";
+    echo "<br>$_naam";
+    //$auto's = "BMW";
+    //echo "<br>$auto's";       //geen hoge komma's
+    //$auto/onderdelen = "Motor";   //geen slashes
+    //echo "<br>$auto/onderdelen";
+    //$huis&tuin = "Tuin";   //geen & teken
+    //echo "<br>$huis&tuin";
+    $naw = "Adres";
+    echo "<br>$naw";
+    //$22 = "getal";    //geen getal
+    //echo "<br>$22";
+    //$$auto = "BMW";  //geen dubbele $ teken
+    //echo "<br>$$auto";
+    $kleurVanAuto = "Blauw";
+    echo "<br>$kleurVanAuto";
+    //$software-dev = "Software Developer";  //geen streepje
+    //echo "<br>$software-dev";
+?>
