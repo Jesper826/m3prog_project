9c88043e9ea553209b77a0e8f23f5731cd051912
JespervanNiekerk <jespervanniekerk1@gmail.com>
2025-02-13 12:19:05 +0100

    create status.php

---
+++ public/01/status.php
@@ -0,0 +1,26 @@
+<?php
+$status = "offline";
+echo "<br>$status";
+$status = "available";
+echo "<br>$status";
+$status = "busy";
+echo "<br>$status";
+$status = "do not disturb";
+echo "<br>$status";
+$status = "offline";
+echo "<br>$status";
+$prijs = "Prijs";
+echo "<br>$prijs";
+$prijs = "20.5";
+echo "<br>$prijs";
+$prijs = "20.1";
+echo "<br>$prijs";
+$prijs = "20.0";
+echo "<br>$prijs";
+$prijs = "20";
+echo "<br>$prijs";
+$prijs = "20.4";
+echo "<br>$prijs";
+$prijs = "20.6";
+echo "<br>$prijs";
+?>
