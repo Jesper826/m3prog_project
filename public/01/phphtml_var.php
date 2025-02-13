8dd33593d29b03fc862cb5fc24555f35c7ee3227
JespervanNiekerk <jespervanniekerk1@gmail.com>
2025-02-13 12:04:03 +0100

    update phphtml_var.php

--- public/01/phphtml_var.php
+++ public/01/phphtml_var.php
@@ -4,22 +4,20 @@
     $datum = date('d-m-Y');
     $tijd = date("H:i");
     $favorieteGame = "Call of Duty";
-    $mmooisteGetal = 7;
-
-    echo $title;
-    echo $datum;
-    echo $tijd;
-    echo $favorieteGame;
-    echo $mmooisteGetal;
+    $mooisteGetal = 7;
+    $favorieteKinderShow = "Spongebob";
+    $leuksteStad = "Amsterdam";
 ?>
 <!doctype html>
 <html lang="en">
     <head>
-        <title><?php echo "Titel van de pagina" ?></title>
+        <title><?php echo $title ?></title>
     </head>
     <body>
-        <h2>De datum vandaag is: <?php echo date('d-m-Y')?>, en de tijd is <?php echo date("H:i")?> uur.
-        
-        <p><h2>Mijn favoriete game</h2><?php echo "bram zijn haarlijn";?></p>
+        <h2>De datum vandaag is: <?php echo $datum?>, en de tijd is <?php echo $tijd?> uur.
+        <p><h2>mooisteGetal:</h2><?=$mooisteGetal ?></p>
+        <p><h2>favorieteKinderShow:</h2><?=$favorieteKinderShow ?></p>w
+        <p><h2>leukste stad</h2><?=$leuksteStad ?></p>
+        <p><h2>Mijn favoriete game</h2><?php echo $favorieteGame;?></p>
     </body>
 </html>
