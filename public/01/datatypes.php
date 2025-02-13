2acbc4550ef5f9266d09de74dca136014e9866e1
JespervanNiekerk <jespervanniekerk1@gmail.com>
2025-02-13 12:40:18 +0100

    datatypes.php added

---
+++ public/01/datatypes.php
@@ -0,0 +1,8 @@
+<?php
+$huisnummer = 123;
+$isGeslaagdRijbewijs = false;
+$benzinePrijs = 1.89;
+echo "Huisnummer: $huisnummer<br>";
+echo "Geslaagd voor rijbewijs: " . ($isGeslaagdRijbewijs ? 'Ja' : 'Nee') . "<br>";
+echo "Prijs per liter Euro 95: €$benzinePrijs<br>";
+?>
