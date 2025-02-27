43cf69ac0720570b299b6856f44fecb4215e2def
JespervanNiekerk <jespervanniekerk1@gmail.com>
2025-02-27 12:40:20 +0100

    Create variabelen.php

---
+++ public/02/variabelen.php
@@ -0,0 +1,19 @@
+<?php
+$naam = 'bram';
+$leeftijd = 16;
+$haarlijn = 'kaal';
+$laatste_cijfer = 0.8;
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p><?php echo $naam . " ". "is". " ". $leeftijd ." ". "hij is". " " .$haarlijn ." "." hij is ". " ". $laatste_cijfer ?></p>
+</body>
+</html>
