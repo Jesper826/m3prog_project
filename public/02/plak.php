44a77f98b6aac76000a57167154c955a8d86371f
JespervanNiekerk <jespervanniekerk1@gmail.com>
2025-02-27 12:06:40 +0100

    Create plak.php

---
+++ public/02/plak.php
@@ -0,0 +1,15 @@
+<?php
+
+$standaardstring ='hello world<br>';
+$extrastring = "extra world <br>";
+
+echo $standaardstring . $extrastring;
+
+$voornaam = 'luigi';
+$achternaam = 'mario';
+$fullname = $voornaam .' '. $achternaam . "<br>";
+
+echo $fullname;
+
+
+?>
