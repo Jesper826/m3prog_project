fc3ebed07e544e042a39261a57a14288a1ad33bc
JespervanNiekerk <jespervanniekerk1@gmail.com>
2025-02-27 12:13:57 +0100

    Update plak.php

--- public/02/plak.php
+++ public/02/plak.php
@@ -11,5 +11,22 @@ $fullname = $voornaam .' '. $achternaam . "<br>";
 
 echo $fullname;
 
+$naam = 'Yoshi';
+$ondertekentDoor = 'bowserjog';
+$enemy = 'koopa';
+
+$emailTekst = "
+Beste $naam<br>
+Helaas moeten wij uw verzoek afwijzen.<br>
+Onze $enemy vinden het niet leuk om opgegeten te worden<br>
+<br>
+Hoogachtend<br>
+$ondertekentDoor<br>
+<br>
+PS:<br>
+De princess is in haar kasteel... voor nu :>
+";
+echo $emailTekst;
+
 
 ?>
