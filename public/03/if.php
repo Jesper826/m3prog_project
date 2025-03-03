a34817b284afd269be73b934bd9a73c4d3808414
JespervanNiekerk <jespervanniekerk1@gmail.com>
2025-03-03 11:43:34 +0100

    Update if.php

--- public/03/if.php
+++ public/03/if.php
@@ -4,11 +4,16 @@ $opvoorraad = false;
 $aantalOpVoorraad = 20;
 $opvoorraad = $aantalOpVoorraad > 0;
 
-if ($aantalOpVoorraad < 0) 
+//if ($opvoorraad == false) 
+if ($aantalOpVoorraad > 0) 
 {
     echo "niet op vorraad";
 
     // hij echode niet wanneer het kleiner dan en gelijk aan 0 is, maar wel wanneer het groter is dan 0
+}
+else
+{
+    echo "Artikel is op voorraad";
 }
 
 
