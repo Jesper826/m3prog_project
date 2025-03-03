57e567ad9b62a4c97d6d5972d43853a85b04d710
JespervanNiekerk <jespervanniekerk1@gmail.com>
2025-03-03 11:37:46 +0100

    Create if.php

---
+++ public/03/if.php
@@ -0,0 +1,16 @@
+<?php
+
+$opvoorraad = false;
+$aantalOpVoorraad = 20;
+$opvoorraad = $aantalOpVoorraad > 0;
+
+if ($aantalOpVoorraad < 0) 
+{
+    echo "niet op vorraad";
+
+    // hij echode niet wanneer het kleiner dan en gelijk aan 0 is, maar wel wanneer het groter is dan 0
+}
+
+
+
+?>
