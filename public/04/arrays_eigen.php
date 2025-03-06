228392e6dd94bf9233c4b9bdefe8f7da427086eb
JespervanNiekerk <jespervanniekerk1@gmail.com>
2025-03-06 16:23:13 +0100

    arrays_eigen en for-loop.php added

--- public/04/arrays_eigen.php
+++ public/04/arrays_eigen.php
@@ -6,6 +6,15 @@ echo "<br>";
 echo $berichten[2];
 echo "<br>";
 echo $berichten[4];
+echo "<br>";
+
+echo count ($berichten). "<br>";
+$berichten[] = "ik ben klein";
+print_r($berichten);
+echo "<br>";
+echo count ($berichten). "<br>";
+sort($berichten);
+echo implode(' >> ', $berichten);
 
 
 ?>
