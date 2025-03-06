ee5f3388da9e85ac267c8c356da82c2ca2699e0f
JespervanNiekerk <jespervanniekerk1@gmail.com>
2025-03-06 16:13:31 +0100

    Create arrays_eigen.php

---
+++ public/04/arrays_eigen.php
@@ -0,0 +1,11 @@
+<?php
+
+$berichten = ["Hallo, hoe gaat het?","Ik ben een beetje moe","Ik heb honger","Ik wil naar huis","Ik wil slapen",];
+var_dump($berichten); 
+echo "<br>";
+echo $berichten[2];
+echo "<br>";
+echo $berichten[4];
+
+
+?>
