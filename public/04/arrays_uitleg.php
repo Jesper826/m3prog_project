f35c5563ae229d0d3cdda06eb89c4a56b0f1fa45
JespervanNiekerk <jespervanniekerk1@gmail.com>
2025-03-06 16:06:15 +0100

    Arrays_uitleg added

---
+++ public/04/arrays_uitleg.php
@@ -0,0 +1,22 @@
+<?php
+
+$nummers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+print_r($nummers);
+echo "<br>";
+echo $nummers[0];
+echo "<br>";
+echo $nummers[4];
+echo "<br>";
+
+
+$dagen = ["maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag", "zondag"];
+echo "<br>";
+echo $dagen[1];
+echo "<br>";
+echo $dagen[2];
+echo "<br>";
+echo $dagen[4];
+echo "<br>";
+echo $dagen[6];
+echo "<br>";
+?>
