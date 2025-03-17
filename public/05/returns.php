51c4542f9a9cb70cd60991dbe605c1820a7b109a
JespervanNiekerk <jespervanniekerk1@gmail.com>
2025-03-17 11:48:21 +0100

    Update returns.php

--- public/05/returns.php
+++ public/05/returns.php
@@ -1,12 +1,26 @@
 <?php
 
-$mijnFunctionResultaat = mijnFunction();
-print($mijnFunctionResultaat);
-
 function mijnFunction()
 {
     return 1;
 }
 
+$mijnFunctionResultaat = mijnFunction();
+echo $mijnFunctionResultaat;
+echo "<br>";
+
+function returnVariable()
+{
+    $result = rand();
+    return $result;
+}
+
+$returnVariableResultaat = returnVariable();
+echo $returnVariableResultaat;
+echo "<br>";
+echo returnVariable();
+echo "<br>";
+echo returnVariable();
+echo "<br>";
 
 ?>
