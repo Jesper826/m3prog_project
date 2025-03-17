c3ac2bb2fa5ea00e44ec41444a08ff58f13c348f
JespervanNiekerk <jespervanniekerk1@gmail.com>
2025-03-17 14:42:17 +0100

    Update vliegkostenfunctions.php

--- public/05/vliegkostenfunctions.php
+++ public/05/vliegkostenfunctions.php
@@ -2,13 +2,10 @@
 
 function berekenVliegKosten($afstand_km, $kerosine_prijs, $bagage_kilo, $business_class) {
     $kosten = ($kerosine_prijs * $afstand_km) / 30;
-    
     $kosten += $bagage_kilo * 5;
-    
     if ($business_class) {
         $kosten *= 1.5;
     }
-    
     return $kosten * 2;
 }
 ?>
