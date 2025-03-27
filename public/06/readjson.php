e990ca438423ee705958bf6cab870f33a7fbc746
JespervanNiekerk <jespervanniekerk1@gmail.com>
2025-03-27 12:02:48 +0100

    Create readjson.php

---
+++ public/06/readjson.php
@@ -0,0 +1,38 @@
+<?
+
+$steden =[
+    "amsterdam",
+    "rotterdam",
+    "den haag"
+];
+
+$jsonString = json_encode($steden);
+$stedenRead = json_decode($jsonString);
+
+print_r($stedenRead);
+
+$json = '{
+    "studenten": [
+      {
+        "name": "mario",
+        "gemiddeldCijfer": 7.5
+      },
+      {
+        "name": "wario",
+        "gemiddeldCijfer": 4.5
+      }
+    ]
+  }';
+
+  $dataObject = json_decode($json);
+  echo "<br>";
+  print_r($dataObject);
+  
+  echo "<br>";
+  echo $dataObject->studenten[0]->name;  
+
+  echo "<br>";
+  echo $dataObject->studenten[0]->gemiddeldCijfer;
+
+
+?>
