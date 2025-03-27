8958546aca7c44d718657fb730edae007074f7a7
JespervanNiekerk <jespervanniekerk1@gmail.com>
2025-03-27 12:08:58 +0100

    Create datametarray.php

---
+++ public/06/datametarray.php
@@ -0,0 +1,20 @@
+<?
+
+header("Content-Type: application/json");
+
+$mario = [ "name" => "mario", "gemiddeldCijfer" => 7.5];
+
+$wario = [ "name" => "wario", "gemiddeldCijfer" => 4.5];
+
+$toad = [ "name" => "toad", "gemiddeldCijfer" => 8.5];
+
+$gumba = [ "name" => "gumba", "gemiddeldCijfer" => 100];
+
+$dataObject = [
+    "studenten" => [$mario, $wario]
+];
+
+$jsonString = json_encode($dataObject);
+echo $jsonString;
+
+?>
