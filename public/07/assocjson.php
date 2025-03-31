fe09f93f1e11100cc90f8c92c6e97f9eae1a6ce1
JespervanNiekerk <jespervanniekerk1@gmail.com>
2025-03-31 12:18:50 +0200

    Create assocjson.php

---
+++ public/07/assocjson.php
@@ -0,0 +1,50 @@
+<?php
+
+$verbruik = [
+    'Koelkast' => 150,
+    'Wasmachine' => 200,
+    'Televisie' => 100,
+    'Magnetron' => 120,
+    'Computer' => 80
+];
+
+$trophiesPerGame = [
+    "Demon's Souls" => 1,
+    "Mario Wonder" => 2,
+    "Rayman Origins" => 3,
+    "Tetris" => 4,
+];
+
+$weer = [
+    'maandag'   => 15,
+    'dinsdag'   => 16,
+    'woensdag'  => 15,
+    'donderdag' => 18,
+    'vrijdag'   => 15,
+    'zaterdag'  => 18,
+    'zondag'    => 21,
+];
+
+$persoon = [
+    'Voornaam' => 'Jan',
+    'Achternaam' => 'Jansen',
+    'Leeftijd' => 30,
+    'Favoriet eten' => 'Pizza',
+    'Minst favoriet eten' => 'Courgette',
+    'Woonplaats' => 'Amsterdam',
+    'Hobby' => 'Voetbal'
+];
+
+$jsonArray = [
+    "Games" => $trophiesPerGame,
+    "Persoon" => $persoon,
+    "Weer" => $weer,
+    "Verbruik" => $verbruik
+];
+
+
+print_r($jsonArray);
+
+
+
+?>
